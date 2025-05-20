84246074e5b067b2a0861f75f11a87720c1071fa
Nikolay Nedelchev <nikolay.nedelchev@egt.com>
2025-05-20 20:17:16 +0300

    Add Ram memory device implementation.

--- emulator/ram/ram.cpp
+++ emulator/ram/ram.cpp
@@ -3,5 +3,69 @@
 namespace cpp6502
 {
 
+Ram::Ram(Address start, Word end)
+    : IMemory()
+    , start_(start)
+    , end_(end)
+{
+    if(start > end)
+    {
+        Error("Ram")
+            .Msg("Ram not valid")
+            .Msg("Start: {}", start)
+            .Msg("End: {}", end)
+            .Throw();
+    }
+    memory_.resize(end - start + 1);
+    for(auto& b : memory_) b = 0;
+}
+
+std::vector<Byte> &Ram::Memory()
+{
+    return memory_;
+}
+
+const std::vector<Byte> &Ram::Memory() const
+{
+    return memory_;
+}
+
+Byte Ram::Read(Address address)
+{
+    if (address < start_ || address > end_)
+    {
+        Error("Ram")
+            .Msg("Read address not valid")
+            .Msg("Address: {}", address)
+            .Msg("Start: {}", start_)
+            .Msg("End: {}", end_)
+            .Throw();
+    }
+
+    return memory_[address - start_];
+}
+
+void Ram::Write(Address address, Byte data)
+{
+    if (address < start_ || address > end_)
+    {
+        Error("Ram")
+            .Msg("Write address not valid")
+            .Msg("Address: {}", address)
+            .Msg("Start: {}", start_)
+            .Msg("End: {}", end_)
+            .Msg("Data: {}", int(data))
+            .Throw();
+    }
+
+    memory_[address - start_] = data;
+}
+
+std::string Ram::ToString() const
+{
+    return fmt::format("[Ram] start: {}, end: {}, buffsize: {}",
+                       start_, end_, memory_.size());
+}
+
 }
 
--- emulator/ram/ram.h
+++ emulator/ram/ram.h
@@ -1,13 +1,27 @@
 #pragma once
+#include <common/common.h>
 #include <membus/membus.h>
 
 namespace cpp6502
 {
 
-class Ram
+class Ram : IMemory
 {
 public:
+    Ram(Address start, Word end);
 
+    std::vector<Byte>& Memory();
+    const std::vector<Byte>& Memory() const;
+
+    // IMemory interface
+    Byte Read(Address);
+    void Write(Address, Byte);
+    std::string ToString() const;
+
+private:
+    Address start_;
+    Address end_;
+    std::vector<Byte> memory_;
 };
 
 }
