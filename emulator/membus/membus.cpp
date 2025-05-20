0a7d0013af9bbe844fa012a9cdd73fe17237a4cb
Nikolay Nedelchev <nikolay.nedelchev@egt.com>
2025-05-20 20:00:01 +0300

    Add membus device implementation.

--- emulator/membus/membus.cpp
+++ emulator/membus/membus.cpp
@@ -5,5 +5,109 @@
 namespace cpp6502
 {
 
+void Membus::Clock()
+{
+    isReady_ = true;
+}
+
+Byte Membus::Read(Address address)
+{
+    if (!isReady_)
+    {
+        Error("Memory not ready for read")
+            .Msg("Address: {}", address)
+            .Throw();
+    }
+    isReady_ = false;
+
+    for(auto& slot : slots_)
+    {
+        if (slot.start <= address && slot.end >= address)
+        {
+            lastValildData_ = slot.memory->Read(address);
+            return lastValildData_;
+        }
+    }
+
+    fmt::println("[membus warning] memory device not found, read failed, address: {}", address);
+    return lastValildData_;
+}
+
+void Membus::Write(Address address, Byte data)
+{
+    if (!isReady_)
+    {
+        Error("Memory not ready for write")
+            .Msg("Address: {}", address)
+            .Throw();
+    }
+    isReady_ = false;
+
+    for(auto& slot : slots_)
+    {
+        if (slot.start <= address && slot.end >= address)
+        {
+            return slot.memory->Write(address, data);
+        }
+    }
+
+    fmt::println("[membus warning] memory device not found, write failed, address: {}", address);
+}
+
+std::string Membus::ToString() const
+{
+    std::string str;
+    str += fmt::format("Is readt: {}, ", isReady_);
+    str += fmt::format("Last valid data: {}\n", lastValildData_);
+    for(const auto& slot : slots_)
+    {
+        str += fmt::format("Slot start: {}, Slot end: {}, Device: {}\n",
+                           slot.start, slot.end, slot.memory->ToString());
+    }
+    return str;
+}
+
+void Membus::Connect(IMemory *mem, Address startAddress, Address endAddress)
+{
+    slots_.push_back({mem, startAddress, endAddress});
+    std::sort(slots_.begin(), slots_.end(),
+    [](const Slot& a, const Slot& b) {
+        return a.start < b.start;
+    });
+
+    for (size_t i = 0; i < slots_.size(); ++i)
+    {
+        bool isInvalid = (slots_[i].start > slots_[i].end);
+        bool isOverlap = (i > 0 && slots_[i].start <= slots_[i - 1].end);
+
+        if (isInvalid || isOverlap)
+        {
+            Error("Slot Error")
+                .Msg("Index: {}", i)
+                .Msg("Start: {}", slots_[i].start)
+                .Msg("End: {}", slots_[i].end)
+                .Msg("Is Invalid: {}", isInvalid)
+                .Msg("Is Overlap: {}", isOverlap)
+                .Throw();
+        }
+    }
+}
+
+void Membus::Disconnect(IMemory *mem)
+{
+    auto sz = slots_.size();
+    slots_.erase(
+        std::remove_if(slots_.begin(), slots_.end(),
+        [mem](const Slot& s) {
+           return s.memory == mem;
+        }),
+        slots_.end());
+
+    if (sz == slots_.size())
+    {
+        fmt::println("[membus warning] memory device not found and not removed");
+    }
+}
+
 }
 
--- emulator/membus/membus.h
+++ emulator/membus/membus.h
@@ -4,5 +4,38 @@
 namespace cpp6502
 {
 
+// Assemble
+class IMemory
+{
+public:
+    virtual ~IMemory();
+    virtual Byte Read(Address) = 0;
+    virtual void Write(Address, Byte) = 0;
+    virtual std::string ToString() const = 0;
+};
+
+class Membus : public IMemory, public IDevice
+{
+public:
+
+    // IDevice
+    void Clock() final;
+
+    // IMemory
+    Byte Read(Address) final;
+    void Write(Address, Byte) final;
+    std::string ToString() const final;
+
+    void Connect(IMemory* mem, Address startAddress, Address endAddress);
+    void Disconnect(IMemory* mem);
+
+private:
+
+    struct Slot{IMemory* memory; Address start; Address end;};
+    std::vector<Slot> slots_;
+    Byte lastValildData_ = 0;
+    bool isReady_ = false;
+};
+
 }
 
