d4b1276c5674542528dc25da37dca06f3401c5d0
Nikolay Nedelchev <nikolay.nedelchev@egt.com>
2025-05-21 08:51:25 +0300

    Add code-gen app for generating opcodes cpp file.

---
+++ apps/opcodes-codegen/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <fmt/format.h>
+#include <common/common.h>
+
+namespace cpp6502::opcodes_codegen
+{
+const std::string& GetOpcodesJsonString();
+
+int Main(int /*argc*/, char* /*argv*/[])
+{
+    fmt::println("Hello world");
+    Json opcodes = Json::parse( GetOpcodesJsonString() );
+
+    fmt::println("Opcodes dump:\n{}", opcodes.dump(3));
+
+    fmt::println("\n--------------------\nGoodbye\n");
+
+    return 0;
+}
+
+}
+
+int main(int argc, char* argv[]){return cpp6502::opcodes_codegen::Main(argc, argv);}
