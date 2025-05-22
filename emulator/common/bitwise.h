e0a5e3e67d1fa855c6d5e55a9b76959361cfe182
Nikolay Nedelchev <nikolay.nedelchev@egt.com>
2025-05-22 14:10:35 +0300

    Add bitwise helpers

---
+++ emulator/common/bitwise.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "common.h"
+
+namespace cpp6502
+{
+#define SIC static inline constexpr
+struct Bitwise
+{
+
+    SIC Byte    Bit         (Byte byte, Byte bit)               noexcept {return ((byte & (1 << bit)) >> bit);}
+    SIC Byte    SetBit      (Byte byte, Byte bit, Byte value)   noexcept {return (value ? (byte | (1 << bit)) : (byte & ~(1 << bit)));}
+    SIC Byte    Sign        (Byte byte)                         noexcept {return Bit(byte, 7);}
+    SIC Word    BytesToWord (auto bytes)                        noexcept {return (Word(bytes[0]) | (Word(bytes[1]) << 8));}
+    SIC Byte    HiByte      (Word word)                         noexcept {return Byte(word >> 8);}
+    SIC Byte    LoByte      (Word word)                         noexcept {return Byte(word);}
+    SIC Word    ClrHiByte   (Word word)                         noexcept {return (word & Word(0xff));}
+    SIC Word    ClrLoByte   (Word word)                         noexcept {return (word & (Word(0xff) << 8));}
+};
+
+}
