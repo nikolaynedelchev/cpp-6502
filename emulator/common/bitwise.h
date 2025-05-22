47d87a027c5edf727d23a507c852c5c13f1e43fb
Nikolay Nedelchev <nikolay.nedelchev@egt.com>
2025-05-22 19:50:41 +0300

    Change SetBit function

--- emulator/common/bitwise.h
+++ emulator/common/bitwise.h
@@ -8,7 +8,7 @@ struct Bitwise
 {
 
     SIC Byte    Bit         (Byte byte, Byte bit)               noexcept {return ((byte & (1 << bit)) >> bit);}
-    SIC Byte    SetBit      (Byte byte, Byte bit, Byte value)   noexcept {return (value ? (byte | (1 << bit)) : (byte & ~(1 << bit)));}
+    SIC void    SetBit      (Byte&byte, Byte bit, Byte value)   noexcept {byte = (value ? (byte | (1 << bit)) : (byte & ~(1 << bit)));}
     SIC Byte    Sign        (Byte byte)                         noexcept {return Bit(byte, 7);}
     SIC Word    BytesToWord (auto bytes)                        noexcept {return (Word(bytes[0]) | (Word(bytes[1]) << 8));}
     SIC Byte    HiByte      (Word word)                         noexcept {return Byte(word >> 8);}
