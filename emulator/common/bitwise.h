3fd036ae31e0ddac8d6793013dd54816b9f49b5b
Nikolay Nedelchev <nikolay.nedelchev@egt.com>
2025-05-23 11:05:50 +0300

    Add more resources, add bitwise functions, rename

--- emulator/common/bitwise.h
+++ emulator/common/bitwise.h
@@ -7,14 +7,17 @@ namespace cpp6502
 struct Bitwise
 {
 
-    SIC Byte    Bit         (Byte byte, Byte bit)               noexcept {return ((byte & (1 << bit)) >> bit);}
-    SIC void    SetBit      (Byte&byte, Byte bit, Byte value)   noexcept {byte = (value ? (byte | (1 << bit)) : (byte & ~(1 << bit)));}
-    SIC Byte    Sign        (Byte byte)                         noexcept {return Bit(byte, 7);}
+    SIC Byte    Bit8        (Byte byte, Byte bit)               noexcept {return ((byte >> 1) & 1);}
+    SIC Byte    TestBit8    (Byte byte, Byte bit)               noexcept {return (byte & (1 << bit));}
+    SIC void    SetBit8     (Byte&byte, Byte bit, Byte value)   noexcept {byte = (value ? (byte | (1 << bit)) : (byte & ~(1 << bit)));}
+    SIC Byte    Sign8       (Byte byte)                         noexcept {return Bit8(byte, 7);}
     SIC Word    BytesToWord (auto bytes)                        noexcept {return (Word(bytes[0]) | (Word(bytes[1]) << 8));}
-    SIC Byte    HiByte      (Word word)                         noexcept {return Byte(word >> 8);}
-    SIC Byte    LoByte      (Word word)                         noexcept {return Byte(word);}
-    SIC Word    ClrHiByte   (Word word)                         noexcept {return (word & Word(0xff));}
-    SIC Word    ClrLoByte   (Word word)                         noexcept {return (word & (Word(0xff) << 8));}
+    SIC Byte    HiByte16    (Word word)                         noexcept {return Byte(word >> 8);}
+    SIC Byte    LoByte16    (Word word)                         noexcept {return Byte(word);}
+    SIC void    SetHiByte16 (Word&word, Byte byte)              noexcept {word &= Word(0xff); word |= (Word(byte) << 8);}
+    SIC void    SetLoByte16 (Word&word, Byte byte)              noexcept {word &= (Word(0xff) << 8); word |= Word(byte);}
+    SIC Word    ClrHiByte16 (Word word)                         noexcept {return (word & Word(0xff));}
+    SIC Word    ClrLoByte16 (Word word)                         noexcept {return (word & (Word(0xff) << 8));}
 };
 
 }
